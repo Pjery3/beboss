fcbb1cf87b19bc9d927926eceb7822a093f27c48
Pjery3 <mixxer1265755@gmail.com>
2021-09-01 23:45:28 +0300

    added header&&navbar

--- gulpfile.js
+++ gulpfile.js
@@ -46,7 +46,10 @@ function scripts() {
 }
 
 function styles() {
-    return src('app/scss/style.scss')
+    return src([
+        'app/scss/style.scss',
+        'node_modules/normalize.css/normalize.css'
+    ])
         .pipe(scss({ outputStyle: 'compressed' }))
         .pipe(concat('style.min.css'))
         .pipe(autoprefixer({
