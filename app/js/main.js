f096f67792f1172e1dd06c81d743e87990dc19e8
Pjery3 <mixxer1265755@gmail.com>
2021-09-08 13:25:43 +0300

    added adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,23 @@
-$(function(){
+let windowWidth = window.innerWidth
+$(function () {
+    $(window).on('load', function () {
+        console.log('ww', windowWidth)
+        if (windowWidth < 1370) {
+            $('.content .nav').removeClass('active')
+        }
+        if (windowWidth < 451) {
+            $('#switcher-label').prop('checked', false);
+        }
 
+    })
+    $(window).on('resize', function () {
+        windowWidth = window.innerWidth;
+        windowWidth >= 1370 ? $('.content .nav').addClass('active') : $('.content .nav').removeClass('active');
+    })
+    $('body').on('click', '.content .nav', function (e) {
+        if (windowWidth < 1370) $(this).toggleClass('active');
+    })
+    $('body').on('click', '.header__burger', function(e){
+        $('.nav').toggleClass('active')
+    })
 });
